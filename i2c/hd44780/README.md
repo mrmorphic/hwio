9c00af9c591b7920ab130110592fad9a43b8996b
Mark Stephens <mr.morphic@gmail.com>
2014-01-02 15:24:28 +1300

    added HD44780 over i2c

---
+++ i2c/hd44780/README.md
@@ -0,0 +1,67 @@
+# HD44780 over I2C
+
+This provides control of an HD44780-compatible device that is connected via an I2C expander.
+
+# Usage
+
+Import the packages:
+
+	// import the require modules
+	import(
+		"hwio"
+		"hwio/i2c/hd44780"
+	)
+
+Initialise by fetching an i2c module from the driver, and enabling it. You can get instances of devices attached to
+the bus.
+
+	// Get the i2c module from the driver. This is an example for the BeagleBone Black, which exposes i2c2.
+	m, e := hwio.GetModule("i2c2")
+
+	// Assert that it is an I2C module
+	i2c := m.(I2CModule)
+
+	// Enable the i2c module
+	i2c.Enable()
+
+Get the HD44780 device, so you make requests of it:
+
+	// Get a display device on this i2c bus
+	display := hd44780.NewHD44780(i2c)
+
+	// Initialise the display with the size of display you have.
+	display.Init(20, 4)
+
+	// The display may not show anything if the backlight is turned off
+	display.SetBacklight(true)
+
+	// If you want to see the cursor, you can turn it on
+	display.Cursor()
+
+To display things, you can:
+
+	// clear the display
+	display.Clear()
+
+	// Set cursor back to (0,0)
+	display.Home()
+
+	// Set cursor to a specific column and row (both zero based)
+	display.SetCursor(0, 1)  // second line
+
+	// output a single character
+	display.Data('A')
+
+	// Use any function that expects a Writer to output to the display. This is because the HD44780 type
+	// implements Writer interface.
+	fmt.Fprintf(display, "Hi, %s", name)
+
+Note that characters that are output to the device are not necessarily displayed consequetively. In particular wrapping may not work
+as you expect. This is because of how the display unit maps it's display buffer to positions on the screen. This is described in
+the datasheet for the HD44780 unit.
+
+# Notes
+
+This has been tested on an mjkdz i2c expander and a 20x4 character display, and works correctly. Other LCD i2c expanders
+may map En, Rw and Rs pins differently, however. If you have issues with a different I2C device, let me know, or submit
+a pull request with the configuration that works for you.
---
+++ i2c/hd44780/hd44780_i2c.go
@@ -0,0 +1,284 @@
+// An implementation of the HD44780 display communicating through an I2C expander. This is based on the I2C display library for
+// Arduino: http://hmario.home.xs4all.nl/arduino/LiquidCrystal_I2C/
+//
+// The hardware takes the form of an HD44780 that is connected to I2C by a port expander such as an PCF8574, an 8-bit port expander.
+// Typically it is connected to the display unit using 4 bits for data, and the other bits for RS, RW, EN and backlight control.
+
+// This has been tested against a mjkdz brand adaptor, and works correctly. In other bits of Arduino code the En, Rw and Rs bits were assigned to
+// the port expander differently. The display I tested was a 20x4 unit. Note characters output to the display are not necessarily displayed
+// adjacently; you need to understand how the DRAM on the display maps to characters on the display.
+
+package hd44780
+
+import (
+	"hwio"
+)
+
+const (
+	// commands
+	LCD_CLEARDISPLAY   byte = 0x01
+	LCD_RETURNHOME     byte = 0x02
+	LCD_ENTRYMODESET   byte = 0x04
+	LCD_DISPLAYCONTROL byte = 0x08
+	LCD_CURSORSHIFT    byte = 0x10
+	LCD_FUNCTIONSET    byte = 0x20
+	LCD_SETCGRAMADDR   byte = 0x40
+	LCD_SETDDRAMADDR   byte = 0x80
+
+	// flags for display entry mode
+	LCD_ENTRYRIGHT          byte = 0x00
+	LCD_ENTRYLEFT           byte = 0x02
+	LCD_ENTRYSHIFTINCREMENT byte = 0x01
+	LCD_ENTRYSHIFTDECREMENT byte = 0x00
+
+	// flags for display on/off control
+	LCD_DISPLAYON  byte = 0x04
+	LCD_DISPLAYOFF byte = 0x00
+	LCD_CURSORON   byte = 0x02
+	LCD_CURSOROFF  byte = 0x00
+	LCD_BLINKON    byte = 0x01
+	LCD_BLINKOFF   byte = 0x00
+
+	// flags for display/cursor shift
+	LCD_DISPLAYMOVE byte = 0x08
+	LCD_CURSORMOVE  byte = 0x00
+	LCD_MOVERIGHT   byte = 0x04
+	LCD_MOVELEFT    byte = 0x00
+
+	// flags for function set
+	LCD_8BITMODE byte = 0x10
+	LCD_4BITMODE byte = 0x00
+	LCD_2LINE    byte = 0x08
+	LCD_1LINE    byte = 0x00
+	LCD_5x10DOTS byte = 0x04
+	LCD_5x8DOTS  byte = 0x00
+
+	// flags for backlight control
+	LCD_BACKLIGHT   byte = 0x00
+	LCD_NOBACKLIGHT byte = 0x80
+
+	En byte = 0x10 // B00010000 // Enable bit
+	Rw byte = 0x20 // B00100000 // Read/Write bit
+	Rs byte = 0x40 // B01000000 // Register select bit
+	// En byte = 0x40 // B00010000 // Enable bit
+	// Rw byte = 0x20 // B00100000 // Read/Write bit
+	// Rs byte = 0x10 // B01000000 // Register select bit
+)
+
+type HD44780 struct {
+	device          hwio.I2CDevice
+	displayFunction byte
+	displayControl  byte
+	displayMode     byte
+	numLines        int
+	backlight       byte
+}
+
+func NewHD44780(module hwio.I2CModule, address int) *HD44780 {
+	device := module.GetDevice(address)
+	result := &HD44780{device: device}
+
+	return result
+}
+
+func (display *HD44780) Init(cols int, lines int) {
+	display.displayFunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS
+
+	if lines > 1 {
+		display.displayFunction |= LCD_2LINE
+	}
+	display.numLines = lines
+
+	// for some 1 line displays you can select a 10 pixel high font
+	// if (dotsize != 0) && (lines == 1) {
+	// 	_displayfunction |= LCD_5x10DOTS
+	// }
+
+	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
+	// according to datasheet, we need at least 40ms after power rises above 2.7V
+	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
+	hwio.DelayMicroseconds(50000)
+
+	// Now we pull both RS and R/W low to begin commands
+	display.backlight = LCD_NOBACKLIGHT
+	display.expanderWrite(display.backlight) // reset expanderand turn backlight off (Bit 8 =1)
+	hwio.Delay(1000)
+
+	//put the LCD into 4 bit mode
+	// this is according to the hitachi HD44780 datasheet
+	// figure 24, pg 46
+
+	// we start in 8bit mode, try to set 4 bit mode
+	display.write4bits(0x03)
+	hwio.DelayMicroseconds(4500) // wait min 4.1ms
+
+	// // second try
+	display.write4bits(0x03)
+	hwio.DelayMicroseconds(4500) // wait min 4.1ms
+
+	// // third go!
+	display.write4bits(0x03)
+	hwio.DelayMicroseconds(150)
+
+	// // finally, set to 4-bit interface
+	display.write4bits(0x02)
+
+	// set # lines, font size, etc.
+	display.Command(LCD_FUNCTIONSET | display.displayFunction)
+
+	// turn the display on with no cursor or blinking default
+	display.displayControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
+	display.Display()
+
+	// clear it off
+	display.Clear()
+
+	// Initialize to default text direction (for roman languages)
+	display.displayMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT
+
+	// set the entry mode
+	display.Command(LCD_ENTRYMODESET | display.displayMode)
+
+	display.Home()
+}
+
+func (display *HD44780) Clear() {
+	display.Command(LCD_CLEARDISPLAY) // clear display, set cursor position to zero
+	hwio.DelayMicroseconds(2000)      // this command takes a long time!
+}
+
+func (display *HD44780) Home() {
+	display.Command(LCD_RETURNHOME) // set cursor position to zero
+	hwio.DelayMicroseconds(2000)    // this command takes a long time!
+}
+
+func (display *HD44780) SetCursor(col int, row int) {
+	rowOffsets := []byte{0x00, 0x40, 0x14, 0x54}
+	if row > display.numLines {
+		row = display.numLines - 1 // we count rows starting w/0
+	}
+	display.Command(LCD_SETDDRAMADDR | (byte(col) + rowOffsets[row]))
+}
+
+// Turn the display on/off (quickly)
+func (display *HD44780) NoDisplay() {
+	display.displayControl &= ^LCD_DISPLAYON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+
+func (display *HD44780) Display() {
+	display.displayControl |= LCD_DISPLAYON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+
+// Turns the underline cursor on/off
+func (display *HD44780) NoCursor() {
+	display.displayControl &= ^LCD_CURSORON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+func (display *HD44780) Cursor() {
+	display.displayControl |= LCD_CURSORON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+
+// Turn on and off the blinking cursor
+func (display *HD44780) NoBlink() {
+	display.displayControl &= ^LCD_BLINKON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+func (display *HD44780) Blink() {
+	display.displayControl |= LCD_BLINKON
+	display.Command(LCD_DISPLAYCONTROL | display.displayControl)
+}
+
+// These commands scroll the display without changing the RAM
+func (display *HD44780) ScrollDisplayLeft() {
+	display.Command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT)
+}
+
+func (display *HD44780) ScrollDisplayRight() {
+	display.Command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT)
+}
+
+// This is for text that flows Left to Right
+func (display *HD44780) LeftToRight() {
+	display.displayMode |= LCD_ENTRYLEFT
+	display.Command(LCD_ENTRYMODESET | display.displayMode)
+}
+
+// This is for text that flows Right to Left
+func (display *HD44780) RightToLeft() {
+	display.displayMode &= ^LCD_ENTRYLEFT
+	display.Command(LCD_ENTRYMODESET | display.displayMode)
+}
+
+// This will 'right justify' text from the cursor
+func (display *HD44780) Autoscroll() {
+	display.displayMode |= LCD_ENTRYSHIFTINCREMENT
+	display.Command(LCD_ENTRYMODESET | display.displayMode)
+}
+
+// This will 'left justify' text from the cursor
+func (display *HD44780) NoAutoscroll() {
+	display.displayMode &= ^LCD_ENTRYSHIFTINCREMENT
+	display.Command(LCD_ENTRYMODESET | display.displayMode)
+}
+
+// // Allows us to fill the first 8 CGRAM locations
+// // with custom characters
+// func (display *HD44780) createChar(location byte, charmap byte[]) {
+// 	location &= 0x7; // we only have 8 locations 0-7
+// 	display.Command(LCD_SETCGRAMADDR | (location << 3));
+// 	for i := 0; i < 8; i++ {
+// 		write(charmap[i]);
+// 	}
+// }
+
+func (display *HD44780) SetBacklight(on bool) {
+	if on {
+		display.backlight = LCD_BACKLIGHT
+	} else {
+		display.backlight = LCD_NOBACKLIGHT
+	}
+	display.expanderWrite(0)
+}
+
+func (display *HD44780) Command(command byte) {
+	display.send(command, 0)
+}
+
+func (display *HD44780) Data(data byte) {
+	display.send(data, Rs)
+}
+
+func (display *HD44780) send(data byte, mode byte) {
+	highnib := data >> 4
+	lownib := data & 0x0F
+	display.write4bits(highnib | mode)
+	display.write4bits(lownib | mode)
+}
+
+// write 4 bits, in the low part of the byte
+func (display *HD44780) write4bits(data byte) {
+	display.expanderWrite(data)
+	display.pulseEnable(data)
+}
+
+func (display *HD44780) expanderWrite(data byte) {
+	display.device.WriteByte(data|display.backlight, 0)
+}
+
+func (display *HD44780) pulseEnable(data byte) {
+	display.expanderWrite(data | En) // En high
+	hwio.DelayMicroseconds(1)        // enable pulse must be >450ns
+
+	display.expanderWrite(data & ^byte(En)) // En low
+	hwio.DelayMicroseconds(50)              // commands need > 37us to settle
+}
+
+func (display *HD44780) Write(p []byte) (n int, err error) {
+	for _, b := range p {
+		display.Data(b)
+	}
+	return len(p), nil
+}
