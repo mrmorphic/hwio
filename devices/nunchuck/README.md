ed74bbc22db271a58c9a3e41e53f0ba58d6c1092
Mark Stephens <mr.morphic@gmail.com>
2014-01-13 09:32:28 +1300

    preliminary support for nintendo nunchuck

---
+++ devices/nunchuck/README.md
@@ -0,0 +1,60 @@
+# Nintendo Wii Nunchuck over I2C
+
+This provides a simple way to access the sensor values of a nunchuck that is connected to an i2c bus on your system.
+
+# Usage
+
+Import the packages:
+
+	// import the require modules
+	import(
+		"hwio"
+		"hwio/devices/nunchuck"
+	)
+
+Initialise by fetching an i2c module from the driver. You can get instances of devices attached to
+the bus.
+
+	// Get the i2c module from the driver.
+	m, e := hwio.GetModule("i2c")
+
+	// Assert that it is an I2C module
+	i2c := m.(I2CModule)
+
+Get the nunchuck device, so you make requests of it:
+
+	// Get a gyro device on this i2c bus
+	controller, e := nunchuck.NewNunchuck(i2c)
+	if e != nil {
+		return e
+	}
+
+To get values from the nunchuck, you need to call ReadSensors, which fetches all data from the device, and then use Get
+methods to fetch the properies you want.
+
+	e := controller.ReadSensors()
+
+	// get joystick values (x2 int) from the last call to ReadSensors()
+	joyX, joyY := controller.GetJoystick()
+
+	// get accelerometer values (x3 float) from the last call to ReadSensors()
+	ax, ay, az := controller.GetAccel()
+
+	// get Z button pressed state (bool) from the last call to ReadSensors()
+	zPressed := controller.GetZPressed()
+
+	// get C button pressed state (bool) from the last call to ReadSensors()
+	cPressed := controller.GetCPressed()
+
+	// get roll and pitch values (float32) in degrees from the last call to ReadSensors(). Note these are computed from
+	// the accelerometer values.
+	roll := controller.GetRoll()
+	pitch := controller.GetPitch()
+
+The device joystick and accelerometer values are initially calibrated to observed values on devices, but you can change these.
+
+	// calibrate the center position of the joystick to whatever the last read joystick values were.
+	controller.CalibrateJoystick()
+
+	// Set the zero values for the accelerometer to 3 values.
+	controller.SetAccelZero(zeroX, zeroY, zeroZ)
---
+++ devices/nunchuck/nunchuck.go
@@ -0,0 +1,147 @@
+// Interface for Nintendo Wii nunchucks over I2C.
+// With reference to WiiChuck for Arduino.
+
+package nunchuck
+
+import (
+	"fmt"
+	"hwio"
+	"math"
+)
+
+const (
+	DEVICE_ADDRESS = 0x52
+
+	DEFAULT_JOYSTICK_ZERO_X = 124
+	DEFAULT_JOYSTICK_ZERO_Y = 132
+
+	DEFAULT_ACCEL_ZEROX = 510.0
+	DEFAULT_ACCEL_ZEROY = 490.0
+	DEFAULT_ACCEL_ZEROZ = 460.0
+
+	RADIUS = 210
+)
+
+type Nunchuck struct {
+	device     hwio.I2CDevice
+	zeroJoyX   int
+	zeroJoyY   int
+	zeroAccelX float32
+	zeroAccelY float32
+	zeroAccelZ float32
+
+	lastJoyX     int
+	lastJoyY     int
+	lastAccelX   float32
+	lastAccelY   float32
+	lastAccelZ   float32
+	lastZPressed bool
+	lastCPressed bool
+}
+
+func NewNunchuck(module hwio.I2CModule) (*Nunchuck, error) {
+	device := module.GetDevice(DEVICE_ADDRESS)
+	n := &Nunchuck{device: device}
+
+	n.SetJoystickZero(DEFAULT_JOYSTICK_ZERO_X, DEFAULT_JOYSTICK_ZERO_Y)
+	n.SetAccelZero(DEFAULT_ACCEL_ZEROX, DEFAULT_ACCEL_ZEROY, DEFAULT_ACCEL_ZEROZ)
+
+	// instead of the common 0x40 -> 0x00 initialization, we
+	// use 0xF0 -> 0x55 followed by 0xFB -> 0x00.
+	// this lets us use 3rd party nunchucks (like cheap $4 ebay ones)
+	// while still letting us use official oness.
+	// see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1264805255
+
+	e := device.WriteByte(0xF0, 0x55) // first config register
+	if e != nil {
+		return nil, e
+	}
+
+	hwio.Delay(1)
+
+	e = device.WriteByte(0xFB, 0x00) // second config register
+	if e != nil {
+		return nil, e
+	}
+
+	return n, nil
+}
+
+// Read all sensor values from the nunchuck and reads them into the internal state of the nunchuck instance.
+// Use Get methods to retrieve sensor values since last call of ReadSensors.
+func (n *Nunchuck) ReadSensors() error {
+	// Get bytes from the sensor, packed into 6 bytes.
+	bytes := n.device.Read(0, 6)
+
+	if len(bytes) < 6 {
+		return fmt.Errorf("Error getting nunchuck data, expected 6 bytes but got %d", len(bytes))
+	}
+
+	// Split out the packet into the n.last* variables.
+
+	// bytes[0] and bytes[1] are joystick X and Y respectively
+	n.lastJoyX = int(bytes[0])
+	n.lastJoyY = int(bytes[1])
+
+	// bytes[2] - bytes[4] are accelX, accelY and accelZ most significant byte respectively. LSB are in bytes[5]
+	ax := int(bytes[2]<<2 | (bytes[5] >> 2 & 3))
+	ay := int(bytes[3]<<2 | (bytes[5] >> 4 & 3))
+	az := int(bytes[4]<<2 | (bytes[5] >> 6 & 3))
+
+	n.lastAccelX = ax - n.zeroAccelX
+	n.lastAccelY = ay - n.zeroAccelY
+	n.lastAccelZ = az - n.zeroAccelZ
+
+	n.lastZPressed = false
+	if bytes[5]&1 > 0 {
+		n.lastZPressed = true
+	}
+
+	n.lastCPressed = false
+	if bytes[5]&2 > 0 {
+		n.lastCPressed = true
+	}
+}
+
+// Calibrate the joystick to the most recently read values.
+func (n *Nunchuck) CalibrateJoystick() {
+	n.SetJoystickZero(n.lastJoyX, n.lastJoyY)
+}
+
+// Calibrate the joystick to explicit values.
+func (n *Nunchuck) SetJoystickZero(x int, y int) {
+	n.zeroJoyX = x
+	n.zeroJoyY = y
+}
+
+func (n *Nunchuck) SetAccelZero(x float32, y float32, z float32) {
+	n.zeroAccelX = x
+	n.zeroAccelY = y
+	n.zeroAccelZ = z
+}
+
+func (n *Nunchuck) GetJoystick() (x int, y int) {
+	return n.lastJoyX, n.lastJoyY
+}
+
+func (n *Nunchuck) GetAccel() (ax float, ay float, az float) {
+	return n.lastAccelX, n.lastAccelY, n.lastAccelZ
+}
+
+func (n *Nunchuck) GetZPressed() bool {
+	return n.lastZPressed
+}
+
+func (n *Nunchuck) GetCPressed() bool {
+	return n.lastCPressed
+}
+
+// Read roll in degrees, computed from accelerometer
+func (n *Nunchuck) GetRoll() float {
+	return math.Atan2(n.lastAccelX, n.lastAccelZ) / math.Pi * 180.0
+}
+
+// Read pitch in degrees, computed from accelerometer
+func (n *Nunchuck) GetPitch() float {
+	return math.Acos(n.lastAccelY/RADIUS) / math.Pi * 180.0
+}
