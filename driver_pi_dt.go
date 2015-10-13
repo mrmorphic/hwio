445a77c641ffc2493bcb78efb96024a2a3f945a4
Chris J Arges <christopherarges@gmail.com>
2015-10-12 22:10:28 -0500

    Detect Raspberry PI using Cpuinfo
    Because kernels may not have special strings embedded in uname, detect if we are
    running based on Hardware field in /proc/cpuinfo.
    
    Signed-off-by: Chris J Arges <christopherarges@gmail.com>

--- driver_pi_dt.go
+++ driver_pi_dt.go
@@ -31,17 +31,16 @@ func NewRaspPiDTDriver() *RaspberryPiDTDriver {
 	return &RaspberryPiDTDriver{}
 }
 
-// @todo determine a better way to detect the raspberry pi that is not dependent on uname
 func (d *RaspberryPiDTDriver) MatchesHardwareConfig() bool {
-	uname, e := exec.Command("uname", "-a").Output()
+	cpuinfo, e := exec.Command("cat", "/proc/cpuinfo").Output()
 	if e != nil {
 		return false
 	}
-
-	s := string(uname)
-	if strings.Contains(s, "raspberrypi") || strings.Contains(s, "adafruit") || strings.Contains(s, "alarmpi") {
+	s := string(cpuinfo)
+	if strings.Contains(s, "BCM2708") || strings.Contains(s, "BCM2709") {
 		return true
 	}
+
 	return false
 }
 
